b51390549301de70a6b1ecd2158f979d8c90d205
nycolas lacerda <lacerdanycolas751@gmail.com>
2022-09-12 22:05:56 -0300

    feat: carregando dev

---
+++ index.ts
@@ -0,0 +1,25 @@
+// função com parametros tipados como number aceitam int/float
+function soma(num1: number, num2: number){
+    console.log(num1 + num2);
+}
+
+soma(1, 3.3); // chamada da função
+
+// criação do tipo de objeto Carro, o mesmo é utilizado abaixo
+type Carro = { 
+    id: number;
+    modelo: string;
+    ano: number;
+    fabricante: string;
+};
+
+// utilização do tipo Carro
+const carro: Carro = {
+    id: 1,
+    modelo: "Mustang",
+    ano: 1969,
+    fabricante: "Ford",
+};
+
+console.log(carro);
+console.log(carro.id, carro.modelo, carro.ano, carro.fabricante);
