5008de0c4ca629bf0b898b44c692ab4c526e1cfb
nycolas lacerda <lacerdanycolas751@gmail.com>
2022-09-17 18:34:36 -0300

    feat: atualização do layout da página

--- scripts/loja_jogos/index.ts
+++ scripts/loja_jogos/index.ts
@@ -3,85 +3,100 @@ type Jogo = {
     nome: string;
     img: string; 
     preco: number;
+    generos: string[];
 }
 
 const jogos = [
         {
             id: 1,
             nome: "Bloodborne",
-            img: "../../img/Bloodborne.jpg",
+            img: "../../img/jogos/Bloodborne.jpg",
             preco: 79.90,
+            generos: ['Soulslike', 'RPG', 'Fantasia Sombria'],
         },
         {
             id: 2,
-            nome: "Dark Souls 2 Scholar Of Thge First Sin",
-            img: "../../img/DarkSouls2SOTFS.jpg",
-            preco: 79.90,
+            nome: "Dark Souls Remastered",
+            img: "../../img/jogos/DarkSoulsRemastered.jpg",
+            preco: 129.90,
+            generos: ['Soulslike', 'RPG', 'Fantasia Sombria'],
         },
         {
             id: 3,
-            nome: "Dark Souls 3",
-            img: "../../img/DarkSouls3.jpg",
-            preco: 12.5,
+            nome: "Dark Souls 2 Scholar Of The First Sin",
+            img: "../../img/jogos/DarkSouls2SOTFS.jpg",
+            preco: 79.99,
+            generos: ['Soulslike', 'RPG', 'Fantasia Sombria'],
         },
         {
             id: 4,
-            nome: "Dark Souls Remastered",
-            img: "../../img/DarkSoulsRemastered.jpg",
-            preco: 12.5,
+            nome: "Dark Souls 3",
+            img: "../../img/jogos/DarkSouls3.jpg",
+            preco: 159.90,
+            generos: ['Soulslike', 'RPG', 'Fantasia Sombria'],
+
         },
         {
             id: 5,
-            nome: "Devil May Cry 4",
-            img: "../../img/DevilMayCry4.jpeg",
-            preco: 12.5,
+            nome: "Devil May Cry 4: Special Edition",
+            img: "../../img/jogos/DevilMayCry4.jpeg",
+            preco: 33.56,
+            generos: ['Ação', 'Hack and Slash'],
         },
         {
             id: 6,
             nome: "Devil May Cry 5",
-            img: "../../img/DevilMayCry5.webp",
-            preco: 12.5,
+            img: "../../img/jogos/DevilMayCry5.webp",
+            preco: 32.96,
+            generos: ['Ação', 'Hack and Slash'],
         },
         {
             id: 7,
             nome: "Dragon's Dogma: Dark Arisen",
-            img: "../../img/DragonsDogmaDA.jpg",
-            preco: 12.5,
+            img: "../../img/jogos/DragonsDogmaDA.jpg",
+            preco: 74.99,
+            generos: ['RPG', 'Mundo Aberto'],
         },
         {
             id: 8,
             nome: "Elden Ring",
-            img: "../../img/EldenRing.jpg",
-            preco: 12.5,
+            img: "../../img/jogos/EldenRing.jpg",
+            preco: 249.90,
+            generos: ['RPG', 'Mundo Aberto', 'Soulslike'],
         },
         {
             id: 9,
             nome: "God Of War",
-            img: "../../img/GodOfWar.webp",
-            preco: 12.5,
+            img: "../../img/jogos/GodOfWar.webp",
+            preco: 159.92,
+            generos: ['Ação', 'Aventura', 'RPG'],
         },
         {
             id: 10,
-            nome: "The Elder Scrolls V: Skyrim",
-            img: "../../img/TESVSkyrim.jpg",
-            preco: 12.5,
+            nome: "TES V: Skyrim Special Edition",
+            img: "../../img/jogos/TESVSkyrimSpecialEdition.webp",
+            preco: 149.00,
+            generos: ['RPG', 'Mundo Aberto', 'Aventura'],
         },
         {
             id: 11,
             nome: "The Witcher 3: Wild Hunt",
-            img: "../../img/TheWitcher3WildHunt.webp",
-            preco: 12.5,
+            img: "../../img/jogos/TheWitcher3WildHunt.webp",
+            preco: 79.99,
+            generos: ['RPG', 'Mundo Aberto', 'Atmosférico'],
         },
         {
             id: 12,
-            nome: "The Legends Of Zelda Breath Of The Wild",
-            img: "../../img/TLoZBoTW.jpg",
-            preco: 12.5,
+            nome: "The Legends Of Zelda Breath Of The Wild",    
+            img: "../../img/jogos/TLoZBoTW.jpg",
+            preco: 346.89,
+            generos: ['RPG', 'Mundo Aberto', 'Ação'],
         },
 ];
 
 const rootElement = document.querySelector("#root");
 const searchButtonElement = document.querySelector("#search-button");
+const homeButtonElement = document.querySelector("#home-button");
 const searchInputElement = document.querySelector("#input-pesquisar");
 const searchSelectElement = document.querySelector("#filter-type-select");
 
@@ -90,11 +105,16 @@ function render(itens: Jogo[]){
     if(rootElement){   
         rootElement.innerHTML = '';    
         itens.forEach((item) => {
+
+            
             rootElement.innerHTML += `
                 <div class="item-wrapper">
                     <h3>${item.nome}</h3>
                     <img src="${item.img}" width="100%" height="125px" alt="${item.nome}" title="${item.nome}" >
                     <h3>${new Intl.NumberFormat('pt-BR',  { style: 'currency', currency: 'BRL' }).format(item.preco)}</h3>
+                    <div class="genders">
+                        <p>${item.generos.join(', ')}</p>
+                    </div>
                 </div>
             `;
         });
@@ -103,16 +123,33 @@ function render(itens: Jogo[]){
 
 function search(){
     const searchInputValue = (searchInputElement as HTMLInputElement).value;
-    const filterTypeValue = (searchSelectElement as HTMLSelectElement).value as keyof Pick<Jogo, 'nome'>;
+    const filterTypeValue = (searchSelectElement as HTMLSelectElement).value as keyof Pick<Jogo, 'nome' | 'generos'>;
     // const filterTypeValue2 = (searchSelectElement as HTMLSelectElement).value as keyof Omit<Jogo, 'id' | 'preco'>;
-   
-    const newJogos = jogos.filter((jogo) => jogo[filterTypeValue].toLowerCase().includes(searchInputValue.toLowerCase()));
-    console.log(newJogos);
+    let newJogos: Jogo[] = [];
+    if(filterTypeValue === 'nome'){
+        newJogos = jogos.filter((jogo) => jogo[filterTypeValue].toLowerCase().includes(searchInputValue.toLowerCase()));
+    }
+    if(filterTypeValue === 'generos'){
+        newJogos = jogos.filter((jogo) => {
+            return jogo[filterTypeValue].filter((genero) => {
+                console.log(genero.toLowerCase().includes(searchInputValue.toLowerCase()));
+                
+                return genero.toLowerCase().includes(searchInputValue.toLowerCase());
+                
+            });
+        });
+    }
+    // const newJogos = jogos.filter((jogo) => jogo[filterTypeValue].toLowerCase().includes(searchInputValue.toLowerCase()));
     render(newJogos);
 }
 
+function reset(){
+    render(jogos);
+}
+
 function eventListenerHandle(){
-    (searchButtonElement as HTMLButtonElement)?.addEventListener('click',search)
+    (searchButtonElement as HTMLButtonElement)?.addEventListener('click',search);
+    (homeButtonElement as HTMLDivElement)?.addEventListener('click',reset);
 }
 
 render(jogos);
