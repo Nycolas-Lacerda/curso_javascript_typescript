f94deaaed847a316ca1a8afeab67e346ffb9c4cb
nycolas lacerda <lacerdanycolas751@gmail.com>
2022-10-09 15:15:07 -0300

    feat: subindo novas correções e atualizações

--- scripts/api/index.js
+++ scripts/api/index.js
@@ -1,79 +1,120 @@
 const rootElement = document.querySelector("#main-wrapper");
 const inputNomeElement = document.querySelector("#nome-pesquisa");
 const inputIngredienteElement = document.querySelector("#ingrediente-pesquisa");
+const searchIngredientesButtonElement = document.querySelector("#search-button");
 inputNomeElement === null || inputNomeElement === void 0 ? void 0 : inputNomeElement.addEventListener('input', filtrarNome);
-inputIngredienteElement === null || inputIngredienteElement === void 0 ? void 0 : inputIngredienteElement.addEventListener('input', filtrarIngrediente);
+searchIngredientesButtonElement === null || searchIngredientesButtonElement === void 0 ? void 0 : searchIngredientesButtonElement.addEventListener('click', filtrarIngrediente);
 const response = await fetch(`https://receitas-server.vercel.app/api`);
 const dados = await (response.json());
 const numPorPagina = 30;
-const paginas = Math.floor(dados.length / numPorPagina);
 let paginaAtual = 1;
 const textPageElement = document.querySelector("#actual-page");
 const fastBackwardButtonElement = document.querySelector("#fast-backward");
 const backwardButtonElement = document.querySelector("#backward");
 const forwardButtonElement = document.querySelector("#forward");
 const fastForwardButtonElement = document.querySelector("#fast-forward");
-fastBackwardButtonElement === null || fastBackwardButtonElement === void 0 ? void 0 : fastBackwardButtonElement.addEventListener('click', () => { });
-backwardButtonElement === null || backwardButtonElement === void 0 ? void 0 : backwardButtonElement.addEventListener('click', () => { });
-forwardButtonElement === null || forwardButtonElement === void 0 ? void 0 : forwardButtonElement.addEventListener('click', () => { });
-fastForwardButtonElement === null || fastForwardButtonElement === void 0 ? void 0 : fastForwardButtonElement.addEventListener('click', () => { });
 textPageElement.innerText = `${paginaAtual}`;
 function quebraPesquisa(pesquisa) {
-    return pesquisa.split(';');
+    return pesquisa.trim().split(';');
 }
 async function filtrarNome() {
     const nome = inputNomeElement.value;
-    const filtro = dados.filter((receita) => {
-        console.log(receita.Name.toLowerCase(), nome.toLowerCase());
-        receita.Name.toLowerCase().includes(nome.toLowerCase());
-    });
-    renderReceitas(filtro);
+    const filtro = dados.filter((receita) => receita.Name.toLowerCase().includes(nome.toLowerCase()));
+    renderizarReceitas(filtro);
 }
 async function filtrarIngrediente() {
     const ingrediente = inputIngredienteElement.value;
+    const array = quebraPesquisa(ingrediente);
+    console.log(array);
     const filtro = dados.filter((receita) => {
-        const array = quebraPesquisa(ingrediente);
         const buscaIngrediente = receita.Ingredients.filter((ingredienteReceita) => {
-            return array.filter((ingrediente) => {
-                return ingredienteReceita.toLowerCase().includes(ingrediente.toLowerCase());
+            const arrayBusca = array.filter((ingredientePesquisa) => {
+                return ingredienteReceita.toLowerCase().includes(ingredientePesquisa.toLowerCase());
             });
+            if (arrayBusca.length === array.length) {
+                return receita;
+            }
         });
         if (buscaIngrediente.length) {
-            console.log(1);
+            console.log(receita);
             return receita;
         }
     });
-    renderReceitas(filtro);
+    // const filtro = dados.filter((receita) => {
+    //     const buscaIngrediente = receita.Ingredients.filter((ingredienteReceita) => {
+    //         return array.filter((ingrediente) => {
+    //             console.log(ingredienteReceita, ' -> ', ingrediente);
+    //             return ingredienteReceita.toLowerCase().includes(ingrediente.toLowerCase());
+    //         });
+    //     });
+    //     if(buscaIngrediente.length){
+    //         return receita;
+    //     }
+    // });
+    renderizarReceitas(filtro);
 }
-function paginarReceitas(dados) {
+function calcularPaginas(totalDados) {
+    let calculo = Math.floor(totalDados / numPorPagina);
+    return calculo > 0 ? calculo : 1;
+}
+function paginarReceitas(dados, totalPaginas) {
     let inicio = 0;
     let matrizReceitas = [];
-    for (let i = 0; i < paginas; i++) {
+    for (let i = 0; i < totalPaginas; i++) {
         matrizReceitas[i] = dados.slice(inicio, inicio + numPorPagina);
         inicio += numPorPagina;
     }
     return matrizReceitas;
 }
-async function renderReceitas(dados) {
-    const matrizReceitas = paginarReceitas(dados);
-    if (paginaAtual === 1) {
-        fastBackwardButtonElement.disabled = true;
-        backwardButtonElement.disabled = true;
+async function renderizarReceitas(dados) {
+    console.log(dados);
+    const matrizReceitas = paginarReceitas(dados, calcularPaginas(dados.length));
+    try {
+        if (calcularPaginas(dados.length) > 1) {
+            fastBackwardButtonElement === null || fastBackwardButtonElement === void 0 ? void 0 : fastBackwardButtonElement.addEventListener('click', () => {
+                paginaAtual = 1;
+                renderizarReceitas(dados);
+            });
+            backwardButtonElement === null || backwardButtonElement === void 0 ? void 0 : backwardButtonElement.addEventListener('click', () => {
+                if (paginaAtual > 1) {
+                    paginaAtual--;
+                }
+                renderizarReceitas(dados);
+            });
+            forwardButtonElement === null || forwardButtonElement === void 0 ? void 0 : forwardButtonElement.addEventListener('click', () => {
+                if (paginaAtual < calcularPaginas(dados.length) - 1) {
+                    paginaAtual++;
+                }
+                renderizarReceitas(dados);
+            });
+            fastForwardButtonElement === null || fastForwardButtonElement === void 0 ? void 0 : fastForwardButtonElement.addEventListener('click', () => {
+                paginaAtual = calcularPaginas(dados.length) - 1;
+                renderizarReceitas(dados);
+            });
+        }
+        if (rootElement) {
+            textPageElement.innerText = `${paginaAtual}`;
+            rootElement.innerHTML = '';
+            matrizReceitas[paginaAtual - 1].forEach((dados) => {
+                rootElement.innerHTML += `
+                    <div class="item-wrapper">
+                        <div class="img-wrapper">
+                            <img src="${dados.urlImage}" alt="Imagem da receita" width="100%" height="200px">
+                        </div>
+                        <h2 class="main-text-md center">${dados.Name}</h2>
+                        <p class="main-text-sm right">${dados.Author}</p>
+                        <p class="main-text-sm justify"> <b>Ingredients</b> <br> ${dados.Ingredients}</p>
+                        <p class="main-text-sm justify"> <b>Method</b> <br> ${dados.Method}</p>
+                    </div>`;
+            });
+        }
     }
-    if (rootElement) {
-        rootElement.innerHTML = '';
-        matrizReceitas[paginaAtual].forEach((dados) => {
-            rootElement.innerHTML += `
-                <div class="item-wrapper">
-                    <div class="img-wrapper">
-                        <img src="${dados.urlImage}" alt="Imagem da receita" width="100%" height="225px">
-                    </div>
-                    <h2 class="main-text-md">${dados.Name}</h2>
-                    <p class="main-text-sm">${dados.Author}</p>
-                </div>`;
-        });
+    catch (_a) {
+        if (rootElement) {
+            rootElement.innerHTML = `<h2>Erro ao carregar as receitas...</h2>`;
+        }
     }
 }
 // filtrarIngrediente('butter');
-renderReceitas(dados);
+renderizarReceitas(dados);
 export {};
--- scripts/api/index.ts
+++ scripts/api/index.ts
@@ -3,15 +3,15 @@ import { Receita } from "./types";
 const rootElement = document.querySelector("#main-wrapper");
 const inputNomeElement = document.querySelector("#nome-pesquisa");
 const inputIngredienteElement = document.querySelector("#ingrediente-pesquisa");
+const searchIngredientesButtonElement = document.querySelector("#search-button");
 
 
 inputNomeElement?.addEventListener('input', filtrarNome);
-inputIngredienteElement?.addEventListener('input', filtrarIngrediente);
+searchIngredientesButtonElement?.addEventListener('click', filtrarIngrediente);
 
 const response = await fetch(`https://receitas-server.vercel.app/api`);
 const dados: Receita[] = await (response.json());
 const numPorPagina = 30;
-const paginas = Math.floor(dados.length / numPorPagina);
 let paginaAtual = 1;
 const textPageElement = document.querySelector("#actual-page");
 const fastBackwardButtonElement = document.querySelector("#fast-backward");
@@ -19,77 +19,119 @@ const backwardButtonElement = document.querySelector("#backward");
 const forwardButtonElement = document.querySelector("#forward");
 const fastForwardButtonElement = document.querySelector("#fast-forward");
 
-fastBackwardButtonElement?.addEventListener('click', () => {});
-backwardButtonElement?.addEventListener('click', () => {});
-forwardButtonElement?.addEventListener('click', () => {});
-fastForwardButtonElement?.addEventListener('click', () => {});
-
 (textPageElement as HTMLParagraphElement).innerText = `${paginaAtual}`;
 
 function quebraPesquisa(pesquisa: string){
-    return pesquisa.split(';');
+    return pesquisa.trim().split(';');
 }
 
 async function filtrarNome(){
     const nome = (inputNomeElement as HTMLInputElement).value;
-    const filtro = dados.filter((receita) => {
-        console.log(receita.Name.toLowerCase(), nome.toLowerCase());
-        
-        receita.Name.toLowerCase().includes(nome.toLowerCase());
-    });
-    renderReceitas(filtro);
+    const filtro = dados.filter((receita) => receita.Name.toLowerCase().includes(nome.toLowerCase()));
+    renderizarReceitas(filtro);
 }
 
 async function filtrarIngrediente() {
     const ingrediente = (inputIngredienteElement as HTMLInputElement).value;
+    const array = quebraPesquisa(ingrediente);
+    console.log(array);
+    
     const filtro = dados.filter((receita) => {
-        const array = quebraPesquisa(ingrediente);
-
         const buscaIngrediente = receita.Ingredients.filter((ingredienteReceita) => {
-            return array.filter((ingrediente) => {
-                return ingredienteReceita.toLowerCase().includes(ingrediente.toLowerCase());
+            const arrayBusca = array.filter((ingredientePesquisa) => {
+                return ingredienteReceita.toLowerCase().includes(ingredientePesquisa.toLowerCase());
             });
+            if(arrayBusca.length === array.length){
+                return receita;
+            }
         });
+
         if(buscaIngrediente.length){
-            console.log(1);
+            console.log(receita);
             return receita;
         }
+    }); 
+    // const filtro = dados.filter((receita) => {
 
-    });
-    renderReceitas(filtro);
+    //     const buscaIngrediente = receita.Ingredients.filter((ingredienteReceita) => {
+    //         return array.filter((ingrediente) => {
+    //             console.log(ingredienteReceita, ' -> ', ingrediente);
+    //             return ingredienteReceita.toLowerCase().includes(ingrediente.toLowerCase());
+    //         });
+    //     });
+    //     if(buscaIngrediente.length){
+    //         return receita;
+    //     }
+
+    // });
+    renderizarReceitas(filtro);
 }
 
-function paginarReceitas(dados: Receita[]){
+function calcularPaginas(totalDados: number){
+    let calculo = Math.floor(totalDados / numPorPagina);
+    return calculo > 0 ? calculo : 1;
+}
+
+function paginarReceitas(dados: Receita[], totalPaginas: number){
     let inicio = 0;
     let matrizReceitas = [];  
-    for(let i = 0; i < paginas; i++){
+    for(let i = 0; i < totalPaginas; i++){
         matrizReceitas[i] = dados.slice(inicio, inicio + numPorPagina);
-        inicio += numPorPagina
+        inicio += numPorPagina;
     }
     return matrizReceitas;
 }
 
-async function renderReceitas(dados: Receita[]){
+async function renderizarReceitas(dados: Receita[]){
+    console.log(dados);
     
-    const matrizReceitas = paginarReceitas(dados);
-    if(paginaAtual === 1){
-        (fastBackwardButtonElement as HTMLButtonElement).disabled =true;
-        (backwardButtonElement as HTMLButtonElement).disabled = true;
+    const matrizReceitas = paginarReceitas(dados, calcularPaginas(dados.length));
+    
+    try{
+        if(calcularPaginas(dados.length) > 1){
+            fastBackwardButtonElement?.addEventListener('click', () => {
+                paginaAtual = 1;
+                renderizarReceitas(dados);
+            });
+            backwardButtonElement?.addEventListener('click', () => {
+                if(paginaAtual > 1){
+                    paginaAtual--;
+                }
+                renderizarReceitas(dados);
+            });
+            forwardButtonElement?.addEventListener('click', () => {
+                if(paginaAtual < calcularPaginas(dados.length) - 1){
+                    paginaAtual++;
+                }
+                renderizarReceitas(dados);
+            });
+            fastForwardButtonElement?.addEventListener('click', () => {
+                paginaAtual = calcularPaginas(dados.length) - 1;
+                renderizarReceitas(dados);
+            });
+        }
+        if(rootElement){   
+            (textPageElement as HTMLParagraphElement).innerText = `${paginaAtual}`;    
+            rootElement.innerHTML = '';
+            matrizReceitas[paginaAtual - 1].forEach((dados) => {
+                rootElement.innerHTML += `
+                    <div class="item-wrapper">
+                        <div class="img-wrapper">
+                            <img src="${dados.urlImage}" alt="Imagem da receita" width="100%" height="200px">
+                        </div>
+                        <h2 class="main-text-md center">${dados.Name}</h2>
+                        <p class="main-text-sm right">${dados.Author}</p>
+                        <p class="main-text-sm justify"> <b>Ingredients</b> <br> ${dados.Ingredients}</p>
+                        <p class="main-text-sm justify"> <b>Method</b> <br> ${dados.Method}</p>
+                    </div>`;
+            });        
+        }
     }
-    if(rootElement){       
-        rootElement.innerHTML = '';
-        matrizReceitas[paginaAtual].forEach((dados) => {
-            rootElement.innerHTML += `
-                <div class="item-wrapper">
-                    <div class="img-wrapper">
-                        <img src="${dados.urlImage}" alt="Imagem da receita" width="100%" height="225px">
-                    </div>
-                    <h2 class="main-text-md">${dados.Name}</h2>
-                    <p class="main-text-sm">${dados.Author}</p>
-                </div>`;
-        });
-        
+    catch{
+        if(rootElement){
+            rootElement.innerHTML = `<h2>Erro ao carregar as receitas...</h2>`;
+        }
     }
 }
 // filtrarIngrediente('butter');
-renderReceitas(dados);
+renderizarReceitas(dados);
